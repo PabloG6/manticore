b4aa277b41265c1e625d5dc752b727bf06899241
Pablo Grant <grantpablo4@gmail.com>
2021-09-21 20:36:43 -0500

    feat add generator command to generate controllers

--- tmpl/controller.go
+++ tmpl/controller.go
@@ -7,31 +7,43 @@ type ControllerCommandArgs struct {
 func ControllerTemplate() []byte {
 	return []byte(`
 package controllers	
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 type {{.ControllerName | ToUpperCase}}Controller struct {
 
 }
 
-func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Create() {
+func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Create(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{})
 
 }
 
 
-func ({{.ControllerName | ToLowerCase}} *{{.ControllerName | ToUpperCase}}Controller) Update() {
+func ({{.ControllerName | ToLowerCase}} *{{.ControllerName | ToUpperCase}}Controller) Update(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{})
 
 }
 
 
-func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Delete() {
+func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Delete(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{})
+
 
 }
 
 
-func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Show() {
+func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Show(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{})
 
 }
 
 
-func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Patch() {
+func ({{.ControllerName | ToLowerCase}}Controller *{{.ControllerName | ToUpperCase}}Controller) Patch(c *gin.Context) {
+	c.JSON(http.StatusNoContent, gin.H{})
+
 
 }
 	`)
